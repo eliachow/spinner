a385a14be299514cc9ca1a34a6b25a1d84d3b493
eliachow <eliacchow@gmail.com>
2023-02-19 10:24:57 -0800

    used setTimout() to create spinning animation

---
+++ spinner1.js
@@ -0,0 +1,50 @@
+/*
+---- Spinner I ----
+M1/Wk4/Spinner I: https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w4/activities/388?journey_step=32
+
+---- INSTRUCTIONS ---- 
+
+---- NOTES ----
+We can use the special character \r to return our cursor back to the beginning of the line that we were on. After this, anything we write next to stdout will overwrite the line.
+
+*/
+
+// // testing \r
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+    process.stdout.write('\r|  ');
+}, 1000);
+
+setTimeout(() => {
+    process.stdout.write('\r/  ');
+}, 300);
+
+setTimeout(() => {
+    process.stdout.write('\r—  ');
+}, 500);
+
+setTimeout(() => {
+    // need to escape the backslack since it's a special character.
+    process.stdout.write('\r\\  ');
+}, 700);
+
+setTimeout(() => {
+    process.stdout.write('\r| ');
+}, 900);
+
+setTimeout(() => {
+    process.stdout.write('\r/  ');
+}, 1100);
+
+setTimeout(() => {
+    process.stdout.write('\r—  ');
+}, 1300);
+
+setTimeout(() => {
+    process.stdout.write('\r\\  ');
+}, 1500);
+
+setTimeout(() => {
+    process.stdout.write('\r| ');
+}, 1700);
